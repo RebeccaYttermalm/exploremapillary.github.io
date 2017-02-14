dcdbcf7fe04067e1b2d988a10e15ea218ed6f288
RebeccaYttermalm <rebecca@mapillary.com>
2017-02-14 16:24:46 +0100

    Add files via upload

---
+++ input.js
@@ -0,0 +1,31 @@
+var input = [
+
+//don't edit anything above this line
+//Instructions: to add a new scheduled photo of the date, add the date in "" quotes with format "dd/mm/yyyy" and then the URL in ''
+//To add a new date and URL not listed below, simple add the same format in the next line, with ["dd/mm/yy", 'https://theurl.com/theurl'],
+//And make sure to remember the comma after the brackets! 
+
+["14/02/2017", 'https://www.mapillary.com/app/?focus=photo&pKey=72AXEcT4OkAJZ6YwNd9jKg&lat=32.81871299999999&lng=129.75611700000002&z=17'],
+["15/02/2017", 'https://www.mapillary.com/app/?focus=photo&pKey=b9_wcc5fcC1fYzoWSHFjrA&lat=-16.040813079999992&lng=-69.14784329999996&z=17'],
+["16/02/2017", ''],
+["17/02/2017", ''],
+["18/02/2017", ''],
+["19/02/2017", ''],
+["14/02/2017", ''],
+["20/02/2017", ''],
+["21/02/2017", ''],
+["22/02/2017", ''],
+["23/02/2017", ''],
+["24/02/2017", ''],
+["25/02/2017", ''],
+["26/02/2017", ''],
+["27/02/2017", ''],
+["28/02/2017", ''],
+["01/03/2017", ''],
+["02/03/2017", ''],
+["03/03/2017", ''],
+["04/03/2017", ''],
+
+//don't edit anything below this line
+
+]
